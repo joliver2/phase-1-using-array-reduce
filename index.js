6400be70356c40fa0b210c9d5ac30253e1c8fca3
joliver427@icloud.com <joliver427@icloud.com>
2022-12-28 19:44:28 -0700

    commit tree 13b5be920306d9c2e02917d964e14dd29cada582

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = 0;
 
-// Code your solution here
+batteryBatches.reduce(function(accumulator, element) {
+    totalBatteries = accumulator + element 
+    return totalBatteries
+}, 0)
+
